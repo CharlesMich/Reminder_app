bc370a7cbe2b7f4e9871d0f6de7c91f53c620e4c
CharlesMich <charleedongo@hotmail.com>
2025-02-26 08:20:32 -0600

    Initial Commit

---
+++ Reminders_Observable/Reminders_ObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_ObservableApp.swift
+//  Reminders_Observable
+//
+//  Created by Charles Michael on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_ObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
